f6ec3adbf96fc8c7f999f705b052399201f1c2c0
Levy Santiago <levyssantiago@gmail.com>
2022-12-19 22:21:16 -0300

    refactor: removing bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import ReactDOM from "react-dom/client";
 import Home from "./pages/Home";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
